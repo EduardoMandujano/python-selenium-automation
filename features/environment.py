a527a8f00c5a5783a6646a657f1789c0eb6f253a
EduardoMandujano <ed.j.mandujano@gmail.com>
2023-03-31 04:36:20 -0400

    Homework 9 Modified environment.py

--- features/environment.py
+++ features/environment.py
@@ -2,32 +2,79 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 from app.application import Application
+from selenium.webdriver.support.events import EventFiringWebDriver
+
+from support.logger import logger, MyListener
 
 
-def browser_init(context):
+# Allure command:
+# behave -f allure_behave.formatter:AllureFormatter -o test_results/ features/tests/product_page.feature
+
+
+def browser_init(context, test_name):
 
     """
     :param context: Behave context
+    :param test_name: scenario.name
     """
-    context.driver = webdriver.Chrome()
     service = Service('/Users/eduar/Automation/python-selenium-automation/chromedriver')
+    # service = Service('/Users/eduar/Automation/python-selenium-automation/geckodriver')
     context.driver = webdriver.Chrome(service=service)
-    # context.browser = webdriver.Safari()
-    # context.browser = webdriver.Firefox()
+    # context.driver = webdriver.Firefox(service=service)
+    # context.driver = webdriver.Safari()
+
+    ## HEADLESS MODE ####
+    # options = webdriver.ChromeOptions()
+    # options.add_argument('--headless')
+    # context.driver = webdriver.Chrome(
+    #    chrome_options=options,
+    #    service=service
+    # )
+
+
+    ### EventFiringWebDriver - log file ###
+    ### for drivers ###
+    # context.driver = EventFiringWebDriver(
+    #      webdriver.Chrome(service=service),
+    #      MyListener()
+    #  )
+    # for headless mode ###
+    # context.driver = EventFiringWebDriver(webdriver.Chrome(chrome_options = options), MyListener())
+
+    # for browerstack ###
+    # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    # bs_user = 'eduardomandujano_hQWLx6'
+    # bs_key = 'r87z3v1uSm3tpePpwNq7'
+    #
+    # desired_cap = {
+    #     'browserName': 'Firefox',
+    #     'bstack:options': {
+    #         'os': 'Windows',
+    #         'osVersion': '10',
+    #         'sessionName': test_name
+    #     }
+    # }
+    # url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    # context.driver = webdriver.Remote(url, desired_capabilities=desired_cap)
 
     context.driver.maximize_window()
     context.driver.implicitly_wait(4)
     context.driver.wait = WebDriverWait(context.driver, 10)
-    context.app = Application(driver=context.driver)
+    context.app = Application(context.driver)
 
 
 def before_scenario(context, scenario):
+    # print('\nStarted scenario: ', scenario.name)
+    # browser_init(context)
     print('\nStarted scenario: ', scenario.name)
-    browser_init(context)
+    logger.info(f'Started scenario: {scenario.name}')
+    browser_init(context, scenario.name)
 
 
 def before_step(context, step):
+    # print('\nStarted step: ', step)
     print('\nStarted step: ', step)
+    logger.info(f'Started step: {step}')
 
 
 def after_step(context, step):
